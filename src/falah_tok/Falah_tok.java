0066395ec85a85ba53c4d7043991832f1e4fa793
SBC4 <SBC4@SBC15>
2015-04-17 10:09:08 +0700

    mencoba projek baru

---
+++ src/falah_tok/Falah_tok.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package falah_tok;
+
+/**
+ *
+ * @author SBC4
+ */
+public class Falah_tok {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
