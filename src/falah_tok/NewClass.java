24713a33d544eaf8ac5961bd02546a49e585ab46
SBC4 <SBC4@SBC15>
2015-04-17 10:26:39 +0700

    menjalankan

---
+++ src/falah_tok/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package falah_tok;
+
+/**
+ *
+ * @author SBC4
+ */
+public class NewClass {
+    
+}
